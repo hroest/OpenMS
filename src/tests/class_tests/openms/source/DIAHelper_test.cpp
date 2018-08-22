067bd2717ea41af6965e256d7f969cf3bdf362ef
Hannes Roest <hroest@stanford.edu>
2018-08-22 15:54:15 -0400

    [FIX] test

--- src/tests/class_tests/openms/source/DIAHelper_test.cpp
+++ src/tests/class_tests/openms/source/DIAHelper_test.cpp
@@ -32,7 +32,7 @@
 // $Authors: Witold Wolski, Hannes Roest $
 // --------------------------------------------------------------------------
 
-#include <OpenMS/ANALYSIS/OPENSWATH/DIAHelper.h>
+#include <OpenMS/ANALYSIS/OPENSWATH/DIAScoringHelper.h>
 
 #ifdef USE_BOOST_UNIT_TEST
 
