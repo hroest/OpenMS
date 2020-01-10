6d250a00d70a2a2cd4a5e81d1ba82afc6ec35a7b
Hannes Roest <hannes.rost@utoronto.ca>
2020-01-10 16:10:18 -0500

    old behavior

--- src/openms/source/ANALYSIS/OPENSWATH/DIAScoring.cpp
+++ src/openms/source/ANALYSIS/OPENSWATH/DIAScoring.cpp
@@ -526,6 +526,12 @@ namespace OpenMS
       isotope_dist = solver.estimateFromPeptideWeight(std::fabs(product_mz * putative_fragment_charge));
     }
 
+#ifdef OLD_BEHAVIOR
+    for (IsotopeDistribution::Iterator it = isotope_dist.begin(); it != isotope_dist.end(); ++it)
+    {
+      isotopes.intensity.push_back(it->getIntensity());
+    }
+#else
     double sum = 0; for (const auto& it : isotope_dist) sum += it.getIntensity();
     std::vector<double> isotopes_int;
     for (Size k = 0; k < isotope_dist.size(); k++)
@@ -537,6 +543,7 @@ namespace OpenMS
         isotopes_int.push_back(isotopes_int_tmp[k]);
       }
     }
+#endif
     isotopes.optional_begin = 0;
     isotopes.optional_end = dia_nr_isotopes_;
 
