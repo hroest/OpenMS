ac59217a5395f0828bfdb6a09e5636628d241e93
Hannes Roest <hannes.rost@utoronto.ca>
2021-10-26 17:36:29 -0400

    cleanup

--- src/openms/source/ANALYSIS/ID/sources.cmake
+++ src/openms/source/ANALYSIS/ID/sources.cmake
@@ -49,7 +49,6 @@ foreach(i ${sources_list})
 endforeach(i)
 
 ### pass source file list to the upper instance
-# set(OpenMS_sources ${OpenMS_sources} ${sources})
 set(OpenMS_ANALYSIS_sources ${OpenMS_ANALYSIS_sources} ${sources})
 
 ### source group definition
