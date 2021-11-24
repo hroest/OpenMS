a2e57572e9ff17c1c5e047f4e7540a2d4595cd68
Hannes Roest <hannes.rost@utoronto.ca>
2021-11-24 10:43:09 -0500

    qt moc fix

--- src/openms/include/OpenMS/SYSTEM/sources.cmake
+++ src/openms/include/OpenMS/SYSTEM/sources.cmake
@@ -5,6 +5,7 @@ set(directory include/OpenMS/SYSTEM)
 set(sources_list
 FileWatcher.h
 NetworkGetRequest.h
+ExternalProcess.h
 )
 
 ### add path to the filenames
@@ -20,7 +21,6 @@ source_group("Source Files\\OpenMS\\SYSTEM" FILES ${sources})
 ### list all header files of the directory here
 set(sources_list_h
 BuildInfo.h
-ExternalProcess.h
 File.h
 FileWatcher.h
 JavaInfo.h
