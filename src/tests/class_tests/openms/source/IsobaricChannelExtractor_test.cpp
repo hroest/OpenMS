3479a726f38b1eeb9c9091953f771432da6445b1
Hannes Roest <hannes.rost@utoronto.ca>
2021-11-23 22:22:26 -0500

    header stuff

--- src/tests/class_tests/openms/source/IsobaricChannelExtractor_test.cpp
+++ src/tests/class_tests/openms/source/IsobaricChannelExtractor_test.cpp
@@ -41,8 +41,7 @@
 
 #include <OpenMS/ANALYSIS/QUANTITATION/ItraqFourPlexQuantitationMethod.h>
 #include <OpenMS/ANALYSIS/QUANTITATION/TMTTenPlexQuantitationMethod.h>
-#include <OpenMS/FORMAT/ConsensusXMLFile.h>
-#include <OpenMS/FORMAT/MzDataFile.h>
+#include <OpenMS/KERNEL/ConsensusMap.h>
 #include <OpenMS/FORMAT/MzMLFile.h>
 
 using namespace OpenMS;
