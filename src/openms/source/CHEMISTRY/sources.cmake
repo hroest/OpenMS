3aa30199bead29afaf03f370066a5026468dc1a7
Hannes Roest <hannes.rost@utoronto.ca>
2021-11-24 11:49:46 -0500

    move to BASE

--- src/openms/source/CHEMISTRY/sources.cmake
+++ src/openms/source/CHEMISTRY/sources.cmake
@@ -6,9 +6,6 @@ set(sources_list
 AASequence.cpp
 CrossLinksDB.cpp
 DecoyGenerator.cpp
-Element.cpp
-ElementDB.cpp
-EmpiricalFormula.cpp
 EnzymaticDigestionLogModel.cpp
 EnzymaticDigestion.cpp
 DigestionEnzyme.cpp
@@ -17,16 +14,12 @@ DigestionEnzymeRNA.cpp
 DigestionEnzymeDB.cpp
 ModificationDefinition.cpp
 ModificationDefinitionsSet.cpp
-ModificationsDB.cpp
 ModifiedNASequenceGenerator.cpp
 ModifiedPeptideGenerator.cpp
 NASequence.cpp
 NucleicAcidSpectrumGenerator.cpp
 ProteaseDB.cpp
 ProteaseDigestion.cpp
-Residue.cpp
-ResidueDB.cpp
-ResidueModification.cpp
 RNaseDB.cpp
 RNaseDigestion.cpp
 Ribonucleotide.cpp
@@ -50,6 +43,27 @@ endforeach(i)
 
 ### pass source file list to the upper instance
 set(OpenMS_sources ${OpenMS_sources} ${sources})
+
+### list all filenames of the directory here
+set(sources_list
+Residue.cpp
+ResidueDB.cpp
+ResidueModification.cpp
+ModificationsDB.cpp
+
+Element.cpp
+ElementDB.cpp
+EmpiricalFormula.cpp
+)
+
+### add path to the filenames
+set(sources)
+foreach(i ${sources_list})
+	list(APPEND sources ${directory}/${i})
+endforeach(i)
+
+### pass source file list to the upper instance
+set(OpenMS_BASE_sources ${OpenMS_BASE_sources} ${sources})
 
 ### source group definition
 source_group("Source Files\\CHEMISTRY" FILES ${sources})
